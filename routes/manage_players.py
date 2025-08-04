ecfad516938877b3df8791571c353255e49b09b0
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2025-08-04 15:39:59 +0300

    manage whole image system

--- routes/manage_players.py
+++ routes/manage_players.py
@@ -1,9 +1,16 @@
-# manage_players.py
-
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from database.db import get_db_connection
+import os
+import cloudinary
+import cloudinary.uploader
 
 manage_players_bp = Blueprint('manage_players_bp', __name__, template_folder='templates')
+
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET')
+)
 
 def get_team_players(team_id):
     conn = get_db_connection()
@@ -15,16 +22,28 @@ def get_team_players(team_id):
         WHERE um.team_id = %s
         ORDER BY u.number ASC
     """, (team_id,))
-    players = {
-        row[0]: {
-            'first_name': row[1],
-            'last_name': row[2],
-            'number': row[3],
-            'image': row[4],
-            'player_type': row[5]
+    
+    players = {}
+    for row in cur.fetchall():
+        user_id, first_name, last_name, number, image, player_type = row
+
+        # 🔁 Обработка на пътя към изображението
+        if image:
+            if image.startswith("http"):
+                image_url = image  # Cloudinary линк
+            else:
+                image_url = url_for("static", filename=f"uploads/{image}")
+        else:
+            image_url = None
+
+        players[user_id] = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "number": number,
+            "image": image_url,
+            "player_type": player_type
         }
-        for row in cur.fetchall()
-    }
+
     cur.close()
     conn.close()
     return players
